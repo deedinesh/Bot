544d2665f9b03e353c3c0f4296a6c1fab7c35332
deedinesh <dineshdanny@hotmail.com>
2017-09-17 19:00:34 -0500

    Updated for Sept 18 demo safety point
    Demo safety point

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -15,6 +16,8 @@ var log_reader = require("./modules/log_reader.js");
 var tokenizer = require("./modules/data_mask.js");
 var apiai = require("./modules/apiai_csp.js");
 var csi_api = require("./modules/csi_api.js");
+var account_api = require("./modules/account_api.js");
+var snMissmatch = require("./modules/snMismatchTest.js");
 var config = require('./configuration/config');
 
 app.set('port', process.env.PORT || config.port);
@@ -25,18 +28,45 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+	secret: 'it does not matter',
+	cookie: { secure: false }
+	})
+)
+
+app.get('/accountStatus/:id', account_api.handleAccountStatus);
+
 app.post('/chat', function(req, res)
 {
+	console.log('req.session.account ^^^^^^^^^^^^^^^', req.session.account);
 	var chat_id = req.body.chat_id;
 	var query = req.body.query;
 	var chtbt = req.body.csp;
 	var agent_id = req.body.agent_id;
-	
+
 	console.log("Message from Front End:" + query + "\n");
-	
-	tokenize(chat_id, query, res, chtbt, agent_id);
+
+	tokenize(chat_id, query, res, chtbt, agent_id, req);
 
 });
+
+app.post('/formHandler', function(req, res) {
+
+
+	snMissmatch("this should be the ban").then( (act) => {
+		req.session.account = act;
+		var chat_id = req.body.chat_id;
+		var query = req.body.query; //"ab1234 000456782 1234567892 All Products OOS  sync_no_service Jack Jones"
+		query = query.substring(0, query.lastIndexOf("sync_no_service")+"sync_no_service".length);
+		query = query + " " + act.CRMCustomerFirstName + " " + act.CRMCustomerLastName;
+		var chtbt = req.body.csp;
+		var agent_id = req.body.agent_id;
+
+		console.log("Message from Front End:" + query + "\n");
+
+		tokenize(chat_id, query, res, chtbt, agent_id, req);
+	});}
+);
 
 app.get('/key', function(req, res)
 {
@@ -45,11 +75,11 @@ app.get('/key', function(req, res)
 
 app.post('/view', function(req, res)
 {
-	
+
 	var result = [];
 	// Read the file and print its contents.
 	var filename = 'logs/conversation.log';
-	function func(data) 
+	function func(data)
 	{
 		if(data != "END")
 		{
@@ -65,7 +95,7 @@ app.post('/view', function(req, res)
 			res.send( result);
 		}
 	}
-	 
+
 
 	var input = fs.createReadStream(filename);
 	log_reader.readLines(input, func);
@@ -73,48 +103,48 @@ app.post('/view', function(req, res)
 
 var server = http.createServer(app).listen(app.get('port'), function()
 {
-	require('dns').lookup(require('os').hostname(), function (err, add, fam) 
+	require('dns').lookup(require('os').hostname(), function (err, add, fam)
 	{
 		  var host_url = "http://" + add + ":" + app.get('port');
 		  console.log("The application URL is: " + host_url);
 	})
-	  
+
 });
 
 var mod = require('./modules/sockets.js').initialize(server);
 
-function tokenize(chat_id, query, res, chtbt, agent_id) 
+function tokenize(chat_id, query, res, chtbt, agent_id, req)
 {
 	var msg = tokenizer.mask_data(chat_id, query);
 	console.log("Tokenized String:" + msg + "\n");
-  	send_api_ai_request(chat_id, msg, res, agent_id, query);
+  	send_api_ai_request(chat_id, msg, res, agent_id, query, req);
 }
 
-function send_api_ai_request(chat_id, message, res, agent_id, query)
+function send_api_ai_request(chat_id, message, res, agent_id, query, req)
 {
 	apiai.text_request(chat_id, message, function(reply)
 	{
 		var rp = JSON.parse(reply);
 		console.log("Reply from API AI:" + rp.msg + "\n");
-		detokenize(chat_id, rp.msg, res, agent_id, query, rp.intent);
+		detokenize(chat_id, rp.msg, res, agent_id, query, rp.intent, req);
 	});
 }
 
-function detokenize(chat_id, tokenized_request, res, agent_id, query, intent_name) 
+function detokenize(chat_id, tokenized_request, res, agent_id, query, intent_name, req)
 {
 	var msg = tokenizer.unmask_data(chat_id, tokenized_request);
 	console.log("Dekotenized String:" + msg + "\n");
-	check_message_type(chat_id, msg, res, agent_id, query, intent_name);
+	check_message_type(chat_id, msg, res, agent_id, query, intent_name, req);
 };
 
-var check_message_type = function(chat_id, msg, res, agent_id, query, intent_name)
+var check_message_type = async function(chat_id, msg, res, agent_id, query, intent_name, req)
 {
 	var m = msg.includes("(");
 
 	if (m)
 	{
 		var methodList=msg.substring(msg.lastIndexOf("(")+1,msg.lastIndexOf(")"));
-		
+
 		var n = methodList.startsWith("PROGRESS_MESG");
 		if (n)
 		{
@@ -125,7 +155,7 @@ var check_message_type = function(chat_id, msg, res, agent_id, query, intent_nam
 			var str = methodList.substring(methodList.indexOf("["));
 			var result = invoke_csi_api(str);
 			var op = JSON.parse(result);
-			var event_name = invoke_urls(op.urls);
+			var event_name = await invoke_urls(op.urls, req);
 			apiai.event_request(chat_id, event_name, function(reply)
 			{
 				var rp = JSON.parse(reply);
@@ -138,7 +168,7 @@ var check_message_type = function(chat_id, msg, res, agent_id, query, intent_nam
 			var places = msg.match(/<(.*?)>/g);
 			var result = invoke_csi_api(methodList);
 			var op = JSON.parse(result);
-			var reply = invoke_urls(op.urls);
+			var reply = await invoke_urls(op.urls, req);
 			msg = msg.replace(places, reply);
 			msg = msg.substring(0, msg.indexOf('?')+1);
 			log_and_send(res, chat_id, agent_id, query, msg, intent_name);
@@ -153,7 +183,7 @@ var check_message_type = function(chat_id, msg, res, agent_id, query, intent_nam
 function invoke_csi_api(methodList)
 {
 		var n = methodList.includes("|");
-		
+
 		var urls = [];
 		if (n)
 		{
@@ -169,53 +199,74 @@ function invoke_csi_api(methodList)
 			var ret = create_url(methodList);
 			urls.push(ret);
 		}
-		
+
 		var result = {"urls": urls};
 		return JSON.stringify(result);
-		
-};
 
-function invoke_urls(url_array)
+}
+
+async function invoke_urls(url_array, req)
 {
 	var obj = url_array[0];
-	
-	switch(obj.method) 
-	{
-	    case "get_agent_name":
-	        return csi_api.get_agent_name(obj.value);
+
+	switch(obj.method)
+	{  	
+		case "transfer_address":
+			console.log("----------function call -- transfer_address--------");
+			return {
+				name: "1_0_confirm_ban_yes",
+				//data: {address: __qqq__}
+				data: {address:  req.session.account.CRMServiceAddress}
+			};
+		case "validate_serial_num_status":
+			if ( req.session.account.snMatch ){
+				return {
+					name: "2_0_event_sn_match_confirm",
+					data: {sn: 1000000000}
+				};
+			} else {
+				return {name: "2_1_event_confirm_sn_no"};
+			}
+			break;
+		case "get_agent_name":
+			return {name: csi_api.get_agent_name(obj.value)};
+
+		case "validate_account_status":
+			return {name: csi_api.validate_account_status(obj.value)};
+
+		case "validate_serial_number":
+			return csi_api.validate_serial_number(obj.value);
+
+		case "check_aots_outage":
+			return csi_api.check_aots_outage(obj.value);
+
+	    case "validate_serial_num_status":
+	        return csi_api.validate_serial_num_status(obj.value);
 	        break;
-	    case "validate_account_status":
-	        return csi_api.validate_account_status(obj.value);
-	        break;
-	    case "validate_serial_number":
-	        return csi_api.validate_serial_number(obj.value);
-	        break;
-	    case "check_aots_outage":
-	        return csi_api.check_aots_outage(obj.value);
-	        break;
-	    default:
-	        var text = "I have never heard of that fruit...";
-	    	console.log(text);
-	} 
+	        
+		default:
+			var text = "I have never heard of that fruit...";
+			console.log(text);
+	}
 }
 
 var log_and_send = function(res, chat_id, agent_id, query, reply, intent_name)
 {
 	write_log(chat_id, agent_id, query, reply, intent_name);
-	
+
 	var log_entry = {"chat_id": chat_id, "agent_id": agent_id, "query": query, "reply": reply, "intent": intent_name};
 	conversation_logger.log_message(log_entry);
 	mod.stream_log(JSON.stringify(log_entry));
 	res.send(reply);
 }
 
-function write_log(chat_id, agent_id, query, msg, intent_name) 
+function write_log(chat_id, agent_id, query, msg, intent_name)
 {
 	var url = "http://localhost:8000/write_log?" + "chatID=" + chat_id + "&" + "MsgSentBy=" + agent_id + "&" + "request=" + query + "&" + "response=" + msg + "&" + "intentName=" + intent_name;
-	  
-	  request(url, function (error, response, body) 
+
+	  request(url, function (error, response, body)
 	  {
-		    if (!error && response.statusCode == 200) 
+		    if (!error && response.statusCode == 200)
 		    {
 		    	console.log("Successful Logging in SQLITE");
 		    	console.log(url);
@@ -237,10 +288,10 @@ function create_url(message)
 	var nvp = nv.split(":");
 	var param = nvp[0];
 	var value = nvp[1];
-	
+
 	url["method"] = method;
 	url["param"] = param;
 	url["value"] = value;
-	
+
 	return url;
 }
